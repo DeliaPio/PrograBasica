75d89d2fe4453b19a42ded485929ce6557eff7b3
Camila <78673836+DeliaPio@users.noreply.github.com>
2023-07-05 20:42:33 -0600

    Ataques del jugador

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,6 +1,14 @@
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.addEventListener('click', ataqueAgua)
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click', ataqueTierra)
+
 }
 
 function seleccionarMascotaJugador() {
@@ -33,6 +41,21 @@ function seleccionarMascotaEnemigo(){
     } else {
         spanMascotaEnimigo.innerHTML = 'Squirte'
     }
+}
+
+function ataqueFuego(){
+    ataqueJugador = 'FUEGO'
+    alert(ataqueJugador)
+}
+
+function ataqueAgua(){
+    ataqueJugador = 'AGUA'
+    alert(ataqueJugador)
+}
+
+function ataqueTierra(){
+    ataqueJugador = 'TIERRA'
+    alert(ataqueJugador)
 }
 
 function aleatorio(min, max){
